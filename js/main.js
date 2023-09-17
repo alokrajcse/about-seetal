7768a2eac3354bb98298c3acde181f9db365d48a
alokrajcse <137365623+alokrajcse@users.noreply.github.com>
2023-09-17 20:17:51 +0530

    emailUpdate main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  loadImage("wallpaper");
+  loadImage("pictureImage", "picture");
+
+  // Get references to the button and email address elements
+  var contactButton = document.getElementById("contactButton");
+  var emailContainer = document.getElementById("emailContainer");
+
+  // Add a click event listener to the button
+  contactButton.addEventListener("click", function () {
+    // Toggle the visibility of the email address container with animation
+    if (emailContainer.style.maxHeight) {
+      emailContainer.style.maxHeight = null;
+      setTimeout(function () {
+        emailContainer.style.display = "none";
+      }, 300); // Wait for the collapse animation to finish
+    } else {
+      emailContainer.style.display = "block";
+      emailContainer.style.maxHeight = emailContainer.scrollHeight + "px";
+    }
+  });
+});
+
 function loadImage(id, targetId) {
   var el = document.getElementById(id);
   var targetEl = targetId ? document.getElementById(targetId) : el;
   var imageToLoad;
 
   if (el.dataset.image) {
-    imageToLoad - el.dataset.image;
-    // console.log("1st " + imageToLoad);
-  } else if (typeof el.currentSrc === undefined) {
+    imageToLoad = el.dataset.image;
+  } else if (typeof el.currentSrc === "undefined") {
     imageToLoad = el.src;
-    // console.log("2nd " + imageToLoad);
   } else {
     imageToLoad = el.currentSrc;
-    // console.log("3rd " + imageToLoad);
   }
-  console.log(imageToLoad);
+
   if (imageToLoad) {
     var img = new Image();
     img.src = imageToLoad;
@@ -22,8 +42,3 @@ function loadImage(id, targetId) {
     };
   }
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  loadImage("wallpaper"); 
-  loadImage("pictureImage", "picture");
-});
